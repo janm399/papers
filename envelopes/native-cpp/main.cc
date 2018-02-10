4acd1e66b24f65a936704e883a0c57e5cdc1b821
Jan Machacek <jan.machacek@gmail.com>
2018-02-10 12:33:48 +0000

    C++

--- envelopes/native-cpp/main.cc
+++ envelopes/native-cpp/main.cc
@@ -1,9 +1,30 @@
 #include <iostream>
 #include <easylogging++.h>
+#include "envelopes.pb.h"
+#include "messages.pb.h"
 
 INITIALIZE_EASYLOGGINGPP
 
+using namespace com::acme::envelopes;
+
 int main(int argc, char** argv) {
-    LOG(INFO) << "main()";
+    User user;
+    user.set_date("");
+    user.set_id("asdad");
+    user.set_name("Jan");
+
+    Envelope envelope;
+    envelope.set_correlation_id("");
+    envelope.set_token("43");
+    envelope.mutable_payload()->PackFrom(user);
+
+    LOG(INFO) << envelope.SerializeAsString();
+
+    Envelope envelope2;
+    envelope2.ParseFromString(envelope.SerializeAsString());
+    User user2;
+    envelope2.payload().UnpackTo(&user2);
+
+    LOG(INFO) << user.ShortDebugString();
     return 0;
 }
