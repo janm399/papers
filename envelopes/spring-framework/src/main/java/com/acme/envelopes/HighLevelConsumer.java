8ae09d2b12f1292d7be7f88c5661a65f815a763e
Jan Machacek <jan.machacek@gmail.com>
2018-02-12 10:21:09 +0000

    Works

--- envelopes/spring-framework/src/main/java/com/acme/envelopes/HighLevelConsumer.java
+++ envelopes/spring-framework/src/main/java/com/acme/envelopes/HighLevelConsumer.java
@@ -6,17 +6,10 @@ import org.springframework.stereotype.Service;
 @Service
 public class HighLevelConsumer {
 
-//    @KafkaListener(topicPartitions =
-//        @TopicPartition(topic = "topic-v1", partitionOffsets = @PartitionOffset(partition = "0", initialOffset = "latest"))
-//    )
     @KafkaListener(topics = {"topic-v1"})
-    // TODO: The @Envelope.* annotations are the desired shape of the method, where the values are extracted
-    // TODO: the payload of the ConsumerRecord<..., x>, where x is convertible to Envelope.
-    // TODO: Note in particular the @Envelope.Payload, which performs the google.protobuf.Any conversions
-    void receive(final @Envelope.CorrelationId String correlationId,
-                 final @Envelope.Token String token,
-                 final @Envelope.Payload Messages.User user) {
-
+    void highLevel(final @Envelope.CorrelationId String correlationId,
+                   final @Envelope.Token String token,
+                   final @Envelope.Payload Messages.User user) {
         System.out.println("Received " + correlationId + " => " + token + " => " + user);
     }
 
