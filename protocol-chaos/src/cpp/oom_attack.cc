db940c71896a60de193088d8cc71b815788cf07c
Jan Machacek <jan.machacek@gmail.com>
2018-02-25 19:22:00 +0000

    x

--- protocol-chaos/src/cpp/oom_attack.cc
+++ protocol-chaos/src/cpp/oom_attack.cc
@@ -9,12 +9,13 @@ namespace com::acme::protocolchaos {
 
     void oom_attack() {
         optional<X> x = nullopt;
-        const uint8 malicious[] = {8, 1, 18, 127, 127, 127, 4, 65};
+        const uint8 malicious[] = {8, 65, 18, 127, 127, 127, 4, 65};
         io::CodedInputStream cis(malicious, sizeof(malicious));
         do {
             if (!x) x = X();
-            x->ParsePartialFromCodedStream(&cis);
-            LOG(INFO) << x->greeting().size();
+            auto parsed = x->ParsePartialFromArray(malicious, sizeof(malicious));
+            LOG(INFO) << "Parsed? " << parsed;
+            LOG(INFO) << x->DebugString() << " |greeting|=" <<  x->greeting().size();
         } while (!cis.ConsumedEntireMessage());
         LOG(INFO) << x->DebugString();
     }
