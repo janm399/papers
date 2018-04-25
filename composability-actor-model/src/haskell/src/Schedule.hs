75f3151e1b4a7b79406f21f284262af338a93e48
Jan Machacek <jan.machacek@gmail.com>
2018-04-25 17:18:24 +0100

    With PB

--- composability-actor-model/src/haskell/src/Schedule.hs
+++ composability-actor-model/src/haskell/src/Schedule.hs
@@ -1,14 +1,15 @@
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE TypeOperators #-}
-{-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
-{-# LANGUAGE QuasiQuotes #-}
+{-# LANGUAGE OverloadedStrings #-}
 
 module Schedule(Api, Ctx(..), api, server) where
 
 import qualified Data.ByteString.Lazy as B
 import Control.Monad.IO.Class
 import Servant
+import qualified Proto.Cam.Messages as P
+import Data.ProtoLens.Encoding
 
 class Ctx where
   -- |Finds all scheduled items in some kind of repository
@@ -18,7 +19,7 @@ class Ctx where
 
 type Api =
   "schedule" :> Get '[PlainText] B.ByteString :<|>
-  "schedule" :> ReqBody '[OctetStream, PlainText] B.ByteString :> Post '[JSON] NoContent
+  "schedule" :> ReqBody '[OctetStream, PlainText] B.ByteString :> Post '[OctetStream] B.ByteString
 
 api :: Proxy Api
 api = Proxy
@@ -29,10 +30,10 @@ server = listSchedules :<|> createSchedule
 listSchedules :: Ctx => Handler B.ByteString
 listSchedules = liftIO findAll
 
-createSchedule :: Ctx => B.ByteString -> Handler NoContent
+createSchedule :: Ctx => B.ByteString -> Handler B.ByteString
 createSchedule schedule = do
   liftIO $ save schedule
-  return NoContent
+  return $ B.fromStrict $ encodeMessage (P.User "id" "01-01-2100" "Jan")
 
 instance MimeRender PlainText B.ByteString where
   mimeRender _ val = val
