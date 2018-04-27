87c5a2390ca61f45bb871025dca1313b96664a7e
Jan Machacek <jan.machacek@gmail.com>
2018-04-27 10:50:08 +0100

    CRUD

---
+++ composability-actor-model/src/haskell/src/CRUD.hs
@@ -0,0 +1,18 @@
+module CRUD where
+
+import Data.IORef
+    
+data CRUDService a = CRUDService {
+    -- |Finds all scheduled items in some kind of repository
+    findAll :: IO [a]
+    -- |Adds a new scheduled item to
+    , save :: a -> IO ()
+    }
+
+newInMemoryService :: IO (CRUDService a) 
+newInMemoryService = do
+    s <- newIORef [] :: IO (IORef [a])
+    return $ CRUDService (findAll s) (save s)
+    where
+        findAll = readIORef
+        save s item = modifyIORef' s (\xs -> item : xs)
