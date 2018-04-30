6c8a3419f84956e1d78702397a383da507b5b562
Jan Machacek <jan.machacek@gmail.com>
2018-04-30 16:51:00 +0100

    x

--- composability-actor-model/src/haskell/src/Schedule/Core.hs
+++ composability-actor-model/src/haskell/src/Schedule/Core.hs
@@ -1,52 +1,66 @@
+{-# LANGUAGE FlexibleInstances #-}
 module Schedule.Core where
 
 import Control.Concurrent
 import Control.Concurrent.MVar
 import Data.List
+import Data.Time
 import qualified Proto.Cam.Messages as P
 
 type EntryId = String
-data Entry = Entry {
-    entryId :: String
-} deriving (Show)
 
-type ScheduledEntry = (ThreadId, Entry)
+class Entry a where
+    entryId :: a -> EntryId
+    fireAt :: a -> UTCTime
 
---data Scheduler = Scheduler (MVar [Entry])
-type Scheduler = MVar [ScheduledEntry]
+data ScheduledEntry a = ScheduledEntry ThreadId a 
 
-newScheduler :: IO Scheduler
-addEntry :: Scheduler -> Entry -> IO ()
-removeEntry :: Scheduler -> EntryId -> IO ()
-activeEntries :: Scheduler -> IO [Entry]
+type Schedule a = [ScheduledEntry a]
+type Scheduler a = MVar (Schedule a)
+
+-- Public API
+newScheduler :: Entry a => IO (Scheduler a)
+addEntry :: Entry a => Scheduler a -> a -> IO ()
+removeEntry :: Entry a => Scheduler a -> EntryId -> IO ()
+activeEntries :: Entry a => Scheduler a -> IO [a]
+
+-- Private 
+removeEntryId :: Entry a => EntryId -> Schedule a -> Schedule a
+findEntry :: Entry a => EntryId -> Schedule a -> Maybe (ScheduledEntry a) 
 
 newScheduler = newMVar []
 
 addEntry scheduler entry = do
     entries <- takeMVar scheduler
-    newEntries <- case find (\(t, e) -> (entryId e) == (entryId entry)) entries of
+    newEntries <- case findEntry (entryId entry) entries of
                       Just existing -> return entries
                       Nothing -> do
-                          new <- forkIO $ run entry
-                          return ((new, entry) : entries)
+                          threadId <- forkIO $ run entry
+                          return (ScheduledEntry threadId entry : entries)
     putMVar scheduler newEntries
     where
         run entry = do
             threadDelay 10000000
                     --    ^s ^ ms  
-            putStrLn $ "Running " ++ (entryId entry)
-            modifyMVar_ scheduler (\entries -> return $ filter (\(_, e) -> (entryId e) /= (entryId entry)) entries)
+            let id = (entryId entry)
+            putStrLn $ "Running " ++ id
+            modifyMVar_ scheduler (\entries -> return $ removeEntryId id entries)
 
 removeEntry scheduler id = do
     entries <- takeMVar scheduler
-    newEntries <- case find (\(_, e) -> (entryId e) == id) entries of
-                      Just (t, existing) -> do
-                          killThread t
-                          return $ filter (\(_, e') -> (entryId existing) /= (entryId e')) entries
+    newEntries <- case findEntry id entries of
+                      Just (ScheduledEntry threadId entry) -> do
+                          killThread threadId
+                          return $ removeEntryId id entries 
                       Nothing ->
                           return entries
     putMVar scheduler newEntries
 
 activeEntries scheduler = do
     entries <- readMVar scheduler
-    return $ map snd entries
+    return $ map (\(ScheduledEntry _ entry) -> entry) entries
+
+findEntry id schedule = find (\(ScheduledEntry _ entry) -> (entryId entry) == id) schedule
+
+removeEntryId id = filter (\(ScheduledEntry _ entry) -> (entryId entry) /= id) 
+    
