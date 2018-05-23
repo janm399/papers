fb42ebf5543034da20f9a8bac5e0c2747ea9c61a
Jan Machacek <jan.machacek@gmail.com>
2018-05-23 11:59:12 +0100

    x

--- integrating-microservices/src/cpp/main.cc
+++ integrating-microservices/src/cpp/main.cc
@@ -84,6 +84,26 @@ const string fb4(const int i) noexcept {
     return async(fb1, i).get();
 }
 
+const string fbio1(const int i) noexcept {
+    string fn("/dev/zero");
+    ifstream in(fn);
+    char buffer[1024 * 1024];
+    in.read(buffer, sizeof(buffer));
+    in.close();
+    return fb1(i);
+}
+
+template<typename F>
+auto withIO(const string fn, const F f) {
+    return [fn, f](const int i) {
+        ifstream in(fn);
+        char buffer[1024 * 1024];
+        in.read(buffer, sizeof(buffer));
+        in.close();
+        return f(i);
+    };
+}
+
 template<typename F>
 long run(F f) {
     auto start = system_clock::now();
@@ -102,6 +122,9 @@ int main(int argc, char** argv) {
         run(fb2b);
         run(fb3);
         run(fb4);
+        run(fbio1);
+        run(withIO("/dev/zero", fb1));
+        run(withIO("/tmp/zeros", fb1));
         cout << endl << endl;
     }
 }
