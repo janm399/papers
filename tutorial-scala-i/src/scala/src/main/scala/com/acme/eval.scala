7a6df48094d1001e574a4396026b882768b41c27
Jan Machacek <jan.machacek@gmail.com>
2018-06-09 21:06:18 +0100

    Scala course

--- tutorial-scala-i/src/scala/src/main/scala/com/acme/eval.scala
+++ tutorial-scala-i/src/scala/src/main/scala/com/acme/eval.scala
@@ -1,5 +1,22 @@
 package com.acme
 
+object Expr {
+  import language.implicitConversions
+  trait ExprOps {
+    def self: Expr
+    def plus(right: Expr): Expr = Plus(self, right)
+    def minus(right: Expr): Expr = Minus(self, right)
+    def ÷(right: Expr): Expr = ???
+  }
+
+  implicit class RichInt(const: Int) extends ExprOps {
+    override val self: Expr = Const(const)
+  }
+  implicit class RichExpr(val self: Expr) extends ExprOps
+  implicit def intToExpr(const: Int): Expr = Const(const)
+
+}
+
 sealed trait Expr
 case class Plus(left: Expr, right: Expr) extends Expr
 case class Minus(left: Expr, right: Expr) extends Expr
@@ -20,4 +37,7 @@ object EvaluatorMain extends App {
   val e = Evaluator.eval(Plus(Plus(Const(4), Const(5)), Const(3)))
   println(e)
 
+  import Expr._
+  val ee = Evaluator.eval((5 plus 10) minus 8)
+  println(ee)
 }
