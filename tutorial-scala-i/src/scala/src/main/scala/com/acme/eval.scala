156251c06dd2b34fa6caab2e89e9c6ce3a75e871
Jan Machacek <jan.machacek@gmail.com>
2018-06-11 18:04:05 +0100

    x

--- tutorial-scala-i/src/scala/src/main/scala/com/acme/eval.scala
+++ tutorial-scala-i/src/scala/src/main/scala/com/acme/eval.scala
@@ -5,51 +5,46 @@ import scala.reflect.ClassTag
 
 object Expr {
 
-  trait ExprOps {
-    def self: Expr
-    def plus[A : ClassTag](right: A): Expr = Plus(self, Const(right))
-    def plus(right: Expr): Expr = Plus(self, right)
-    def div(right: Expr): Expr = Div(self, right)
-    def div[A : ClassTag](right: A): Expr = Div(self, Const(right))
-    def minus(right: Expr): Expr = Minus(self, right)
-    def minus[A : ClassTag](right: A): Expr = Minus(self, Const(right))
+  trait ExprOps[A] {
+    def self: Expr[A]
+    def plus(right: A)(implicit ev: ClassTag[A]): Expr[A] = Plus(self, Const(right))
+    def plus(right: Expr[A]): Expr[A] = Plus(self, right)
+    def div(right: Expr[A]): Expr[A] = Div(self, right)
+    def div(right: A)(implicit ev: ClassTag[A]): Expr[A] = Div(self, Const(right))
+    def minus(right: Expr[A]): Expr[A] = Minus(self, right)
+    def minus(right: A)(implicit ev: ClassTag[A]): Expr[A] = Minus(self, Const(right))
   }
 
-  implicit class RichA[A : ClassTag](const: A) extends ExprOps {
-    override val self: Expr = Const(const)
+  implicit class RichA[A : ClassTag](const: A) extends ExprOps[A] {
+    override val self: Expr[A] = Const(const)
   }
-  implicit class RichExpr(val self: Expr) extends ExprOps
+  implicit class RichExpr[A : ClassTag](val self: Expr[A]) extends ExprOps[A]
 
 }
 
-sealed trait Expr
-case class Plus(left: Expr, right: Expr) extends Expr
-case class Minus(left: Expr, right: Expr) extends Expr
-case class Div(left: Expr, right: Expr) extends Expr
-case class Const[A : ClassTag](const: A) extends Expr
-
-trait Applicative[F[_]] {
-  def pure[A](value: A): F[A]
-  def flatMap[A, B](f: A ⇒ B): F[B]
-}
+sealed trait Expr[+A]
+case class Plus[A](left: Expr[A], right: Expr[A]) extends Expr[A]
+case class Minus[A](left: Expr[A], right: Expr[A]) extends Expr[A]
+case class Div[A](left: Expr[A], right: Expr[A]) extends Expr[A]
+case class Const[A : ClassTag](const: A) extends Expr[A]
 
 object Evaluator {
 
-  def eval[A : ClassTag, F[_]](expr: Expr)(implicit N: Fractional[A], F: Applicative[F]): F[A] = expr match {
-    case Plus(l, r) ⇒ for { l ← eval(l); r ← eval(r) } yield N.plus(l, r)
-    case Minus(l, r) ⇒ for { l ← eval(l); r ← eval(r) } yield N.minus(l, r)
-    case Div(l, r) ⇒ for { l ← eval(l); r ← eval(r) } yield N.div(l, r)
-    case Const(c: A) ⇒ F.pure(c)
+  def eval[A : ClassTag](expr: Expr[A])(implicit N: Fractional[A]): A = expr match {
+    case Plus(l, r) ⇒ N.plus(eval(l), eval(r))
+    case Minus(l, r) ⇒ N.minus(eval(l), eval(r))
+    case Div(l, r) ⇒ N.div(eval(l), eval(r))
+    case Const(c: A) ⇒ c
   }
 
 }
 
 object EvaluatorMain extends App {
+  import Expr._
 
-  val e = Evaluator.eval[BigDecimal, Option](Plus(Plus(Const(4), Const(5)), Const(3)))
+  val e: BigDecimal = Evaluator.eval(Const(4))
   println(e)
 
-  import Expr._
-  val ee = Evaluator.eval[Double, Option]((5.4 plus math.Pi) minus 8.8)
+  val ee = Evaluator.eval((5.4 plus math.Pi) minus 8.8)
   println(ee)
 }
