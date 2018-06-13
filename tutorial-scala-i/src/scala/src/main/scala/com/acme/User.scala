f4b9352082912f0bfcbb460a0e45180eb34c9143
Jan Machacek <jan.machacek@gmail.com>
2018-06-13 11:35:43 +0100

    x

---
+++ tutorial-scala-i/src/scala/src/main/scala/com/acme/User.scala
@@ -0,0 +1,28 @@
+package com.acme
+
+import java.util.{Calendar, Date}
+
+class User {
+  var dob: Date = _
+
+  def x(): Unit = {
+    val x = Array(1, 2, 3)
+    !x.indices.exists(idx ⇒ x(idx) != x(idx))
+  }
+
+  def hasBirthday(asOf: Date = new Date()): Boolean = {
+    if (dob != null) {
+      val c1 = Calendar.getInstance()
+      c1.setTime(dob)
+      val dobMonth = c1.get(Calendar.MONTH)
+      val dobDay = c1.get(Calendar.DAY_OF_MONTH)
+
+      val c2 = Calendar.getInstance()
+      c2.setTime(asOf)
+      if (c2.get(Calendar.MONTH) == dobMonth) {
+        c2.get(Calendar.DAY_OF_MONTH) == dobDay
+      } else false
+    } else false
+  }
+
+}
