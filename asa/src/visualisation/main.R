2cbe1e20e31bd1bc2ff80bb8c72e747ba698a6c3
Jan Machacek <jan.machacek@gmail.com>
2018-07-18 21:31:05 +0100

    x

--- asa/src/visualisation/main.R
+++ asa/src/visualisation/main.R
@@ -1,10 +1,18 @@
 library(parcoords)
 library(dplyr)
-asa <- read.csv("~/Desktop/x.tsv", sep = "\t", row.names = NULL)
+library(ggvis)
+asa <- read.csv("~/Desktop/x.tsv", sep = "\t", row.names = "name")
 
-asa %>% 
-  select(score, performance, steps_to_deploy, logging) %>%
+all <- asa %>% 
+  select(score, performance, steps_to_deploy, logging)
+
+all %>%
   parcoords(brushMode = "1d-axes-multi"
             ,reorderable = TRUE
             ,alphaOnBrushed = 0.15
             ,color = list(colorBy = "score", colorScale = htmlwidgets::JS("d3.scale.category10()")))
+
+all %>% ggvis(x = ~score, y = ~steps_to_deploy,
+              size = ~logging, fill = ~performance) %>%
+  layer_points() %>%
+  add_legend(c("size", "fill"))
