5d869c3392dc3fe2ce00e0ad4e830b749833da13
Jan Machacek <jan.machacek@gmail.com>
2018-07-20 09:54:17 +0100

    NAK

--- asa/src/sum/src/main/scala/com/acme/SourceClassifier.scala
+++ asa/src/sum/src/main/scala/com/acme/SourceClassifier.scala
@@ -8,7 +8,7 @@ import nak.liblinear.{LiblinearConfig, SolverType}
 import org.apache.logging.log4j.LogManager
 
 import scala.io.Source
-import scala.util.{Random, Try}
+import scala.util.{Failure, Random, Success, Try}
 
 trait SourceClassifier {
 
@@ -17,46 +17,51 @@ trait SourceClassifier {
 }
 
 object SourceClassifier {
-  import nak.NakContext._
-  private type Classifier = FeaturizedClassifier[String, String]
   private val log = LogManager.getLogger(SourceClassifier.getClass)
-  private val serializedModelFileName = "target/classifier.ser"
 
-  def train(exampleDirectory: Directory): Classifier = {
-    val sources = ExampleLoader.loadLabels(exampleDirectory)
-    log.info(s"Loaded ${sources.length} examples")
-
+  def apply(): SourceClassifier = {
+    import nak.NakContext._
+    type Classifier = FeaturizedClassifier[String, String]
+    val serializedModelFileName = "target/classifier.ser"
     val classes = Seq(
       "akka", "akka-http", "akka-stream", "sbt", "actor", "backpressure", "scalatest", "scalacheck", "validation",
       "amazon-aws", "amazon-dynamo", "kafka", "amazon-kinesis", "amazon-s3", "protobuf", "concurrency", "future",
       "logging", "monitoring"
     )
-    val stopwords = Set("<-", "←", "<:", "<%", "=", "=>", "⇒", ">:", "abstract", "case", "catch", "class", "def", "do", "else", "extends", "false", "final", "finally", "for", "forSome", "if", "implicit", "import", "lazy", "match", "new", "null", "object", "override", "package", "private", "protected", "return", "sealed", "super", "this", "throw", "trait", "true", "try", "type", "val", "var", "while", "with", "yield")
-    val config = LiblinearConfig(cost = 10.0, eps = 0.01, solverType = SolverType.L2R_LR_DUAL, showDebug = true)
-    val featurizer = new BowFeaturizer(stopwords)
-    val trainingExamples = classes.flatMap { cls ⇒
-      val t = sources.filter(_.isTagged(cls))
-      Random.shuffle(t).flatMap(_.sourceCodes().map(Example(cls, _)))
+
+    def train(exampleDirectory: Directory): Classifier = {
+      val sources = ExampleLoader.loadLabels(exampleDirectory)
+      log.info(s"Loaded ${sources.length} examples")
+
+      val stopwords = Set("<-", "←", "<:", "<%", "=", "=>", "⇒", ">:", "abstract", "case", "catch", "class", "def", "do", "else", "extends", "false", "final", "finally", "for", "forSome", "if", "implicit", "import", "lazy", "match", "new", "null", "object", "override", "package", "private", "protected", "return", "sealed", "super", "this", "throw", "trait", "true", "try", "type", "val", "var", "while", "with", "yield")
+      val config = LiblinearConfig(cost = 10.0, eps = 0.01, solverType = SolverType.L2R_LR_DUAL, showDebug = true)
+      val featurizer = new BowFeaturizer(stopwords)
+      val trainingExamples = classes.flatMap { cls ⇒
+        val t = sources.filter(_.isTagged(cls))
+        Random.shuffle(t).flatMap(_.sourceCodes().map(Example(cls, _)))
+      }
+
+      log.info("Training...")
+      val classifier = trainClassifier(config, featurizer, trainingExamples)
+      val oos = new ObjectOutputStream(new FileOutputStream(serializedModelFileName))
+      oos.writeObject(classifier)
+      oos.close()
+      log.info("Training completed.")
+      classifier
     }
 
-    log.info("Training...")
-    val classifier = trainClassifier(config, featurizer, trainingExamples)
-    val oos = new ObjectOutputStream(new FileOutputStream(serializedModelFileName))
-    oos.writeObject(classifier)
-    oos.close()
-    log.info("Training completed.")
-    classifier
-  }
-
-  def apply(alwaysTrain: Boolean): SourceClassifier = {
-    def loadTrained(): Classifier = {
+    def loadTrained(): Try[Classifier] = Try {
       val ois = new ObjectInputStream(new FileInputStream(serializedModelFileName))
       val classifier = ois.readObject().asInstanceOf[Classifier]
       ois.close()
       classifier
+    }.flatMap { classifier ⇒
+      if (classes.forall(classifier.labels.contains)) Success(classifier)
+      else Failure(new RuntimeException("Mismatched classes"))
     }
+
     val exampleDirectory = Directory("/Users/janmachacek/Downloads/so")
-    val classifier = if (alwaysTrain) train(exampleDirectory) else Try(loadTrained()).getOrElse(train(exampleDirectory))
+    val classifier = loadTrained().getOrElse(train(exampleDirectory))
     val minScore = 0.7
 
     new SourceClassifier {
@@ -74,8 +79,9 @@ object SourceClassifier {
 
 
 object M {
+
   def main(args: Array[String]): Unit = {
-    val classifier = SourceClassifier(false)
+    val classifier = SourceClassifier()
 
     Directory("/Users/janmachacek/Sandbox/adengine")
       .findAll(".scala")
